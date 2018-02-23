e775edecf725d148695dfa800b9bb55a146ce687
Andy Ta <me.andyta@gmail.com>
2018-02-23 13:51:12 -0500

    Made Login Modal Longer Not Larger

--- resources/views/modals/login.blade.php
+++ resources/views/modals/login.blade.php
@@ -1,5 +1,5 @@
 <div class="modal fade" id="loginModal" role="dialog" >
-    <div class="modal-dialog">
+    <div class="modal-dialog" style="width: 33%;">
         <form action="/register" id="loginForm">
             <div class="modal-content">
                 <div class="modal-header" style="background: #08306C;">
@@ -9,18 +9,18 @@
                 <div class="modal-body">
                     <div class="container-fluid">
                         <div id="loginError"></div>
-                        <div class="form-group">
+                        <div class="form-group" style="margin-top: 5%; margin-bottom: 5%;">
                             <label>Email</label>
                             <input type="text" class="form-control input-md" placeholder="Email" name="email"/>
                         </div>
-                        <div class="form-group">
+                        <div class="form-group" style="margin-top: 5%; margin-bottom: 5%;">
                             <label>Password</label>
                             <input type="password" class="form-control input-md" placeholder="Password" name="password"/>
                             <!--<span class="pull-right"><a href="#" style="color: grey;">Forgot Password?</a></span>-->
                             <span><a href="#" id="signUpNow" data-toggle="modal" style="color: grey;" data-target="#signupModal">
                                     New to Jumoyo? Signup now</a></span>
                         </div>
-                        <div class="form-group">
+                        <div class="form-group" style="margin-top: 5%; margin-bottom: 5%;">
                             <input style="background: #08306C;" type="submit" id="loginButton" class="btn btn-block btn-md btn-primary" value="LOGIN"/>
                         </div>
                         <!--
