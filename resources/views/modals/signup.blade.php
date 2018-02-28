2f4093e91435a9384785621647f6b892c4c6719f
Andy-Ta <me.andyta@gmail.com>
2018-02-28 00:50:16 -0500

    Message Modal No Show Fix

--- resources/views/modals/signup.blade.php
+++ resources/views/modals/signup.blade.php
@@ -8,50 +8,46 @@
                 <div class="modal-body">
                     <div class="container">
                         <div class="col-sm-8 col-sm-offset-2">
-                        <h2 class="modal-title">Sign up</h2>
-                        <input type="hidden" name="_token" value="{{ csrf_token() }}"><br/>
-                        <div id="displayErrors"></div>
-                        <div class="form-group col-md-6">
-                            <label>First Name</label>
-                            <input type="text" name="firstName" id="firstName" class="form-control input-md"
-                                   placeholder="First name" required/>
+                            <h2 class="modal-title">Sign up</h2>
+                            <input type="hidden" name="_token" value="{{ csrf_token() }}"><br/>
+                            <div id="displayErrors"></div>
+                            <div class="form-group col-md-6">
+                                <label>First Name</label>
+                                <input type="text" name="firstName" id="firstName" class="form-control input-md"
+                                       placeholder="First name" required/>
+                            </div>
+                            <div class="form-group col-md-6">
+                                <label>Last Name</label>
+                                <input type="text" name="lastName" id="lastName" class="form-control input-md"
+                                       placeholder="Last name" required/>
+                            </div>
+                            <div class="form-group col-md-6">
+                                <label>Email</label>
+                                <input type="text" name="email" id="email" class="form-control input-md" placeholder="Email"
+                                       required/>
+                            </div>
+                            <div class="form-group col-md-6">
+                                <label>Password</label>
+                                <input type="password" name="password" id="password" class="form-control input-md"
+                                       placeholder="Password" required/>
+                            </div>
+                            <div class="form-group col-md-6">
+                                <label>Confirm Password</label>
+                                <input type="password" name="password_confirmation" id="password_confirmation"
+                                       class="form-control input-md" placeholder="Confirm Password" required/>
+                            </div>
+                            <div class="form-group col-md-6">
+                                <label>Mobile</label>
+                                <input type="text" name="mobile" id="mobile" class="form-control input-md"
+                                       placeholder="Mobile" required/>
+                            </div>
+                            <div class="form-group col-md-12">
+                                <span class="pull-left" style="color: grey;">By signing up you agree to our <a>terms of service</a> and <a>privacy policy</a></span>
+                            </div>
+                            <div class="form-group col-md-12">
+                                <input id="registerButton" type="submit" class="btn-blue full" value="Sign up"/>
+                            </div>
                         </div>
-                        <div class="form-group col-md-6">
-                            <label>Last Name</label>
-                            <input type="text" name="lastName" id="lastName" class="form-control input-md"
-                                   placeholder="Last name" required/>
-                        </div>
-                        <div class="form-group col-md-6">
-                            <label>Email</label>
-                            <input type="text" name="email" id="email" class="form-control input-md" placeholder="Email"
-                                   required/>
-                        </div>
-                        <div class="form-group col-md-6">
-                            <label>Password</label>
-                            <input type="password" name="password" id="password" class="form-control input-md"
-                                   placeholder="Password" required/>
-                        </div>
-                        <div class="form-group col-md-6">
-                            <label>Confirm Password</label>
-                            <input type="password" name="password_confirmation" id="password_confirmation"
-                                   class="form-control input-md" placeholder="Confirm Password" required/>
-                        </div>
-                        <div class="form-group col-md-6">
-                            <label>Mobile</label>
-                            <input type="text" name="mobile" id="mobile" class="form-control input-md"
-                                   placeholder="Mobile" required/>
-                        </div>
-                        <div class="form-group col-md-12">
-                            <span class="pull-left" style="color: grey;">By signing up you agree to our <a>terms of service</a> and <a>privacy policy</a></span>
-                        </div>
-                        <div class="form-group col-md-12">
-                            <input id="registerButton" type="submit" class="btn-blue full" value="Sign up"/>
-                        </div>
-                        <!--
-                        <div class="form-group">
-                            <input style="background: #3B5998;" type="submit" class="btn btn-block btn-md btn-primary" value="LOGIN WITH FACEBOOK"/>
-                        </div>
-                        -->
                     </div>
                 </div>
             </form>
